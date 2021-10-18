905fe2aa6a4d055b0885977134eeab8a02bf4dd4
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-10-18 09:37:33 +0330

    can change topic

--- MyMQTTFirstLook.Client/Program.cs
+++ MyMQTTFirstLook.Client/Program.cs
@@ -1,32 +1,58 @@
 ﻿using System;
-using Serilog;
 using MQTTnet;
 using System.Text;
 using static System.Console;
-using static Serilog.Log;
 using MQTTnet.Client.Options;
-using MQTTnet;
 using MQTTnet.Client.Connecting;
 using MQTTnet.Client.Disconnecting;
-using MQTTnet.Client.Options;
 using MQTTnet.Extensions.ManagedClient;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Threading;
 
-WriteLine("Hello World!");
+WriteLine("Start App");
+var wait = new ManualResetEvent(false);
+var _url = "";
+var _topic = "dev.to/topic/json";
+do
+{
+    Write("Site Address:");
+     _url = ReadLine();
+} while (string.IsNullOrWhiteSpace(_url));
+var port = 8200;
+do
+{
+    Write("Port number:");
+
+} while (!int.TryParse(ReadLine(), out port));
+WriteLine("Enter topic( if empty use defualt topic):");
+var _tempTopic = ReadLine();
+if (string.IsNullOrWhiteSpace(_tempTopic))
+{
+    WriteLine("use defualt topic: " + _topic);
+}
+else
+    _topic = _tempTopic;
 var builder = new MqttClientOptionsBuilder()
     .WithClientId(Guid.NewGuid().ToString())
-    .WithTcpServer("localhost", 707);
+    .WithTcpServer(_url, port);
 var option = new ManagedMqttClientOptionsBuilder().WithAutoReconnectDelay(TimeSpan.FromSeconds(50)).WithClientOptions(builder.Build()).Build();
 var client = new MqttFactory().CreateManagedMqttClient();
 client.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
 client.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
 client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
-await client.StartAsync(option);
+ client.StartAsync(option).GetAwaiter().GetResult();
+client.UseApplicationMessageReceivedHandler(e =>
+{
+    WriteLine(e.ApplicationMessage.Topic);
+});
 while (true)
 {
-    string json = JsonSerializer.Serialize(new { message = "Heyo :)", sent = DateTimeOffset.UtcNow });
-    await client.PublishAsync("dev.to/topic/json", json);
+    if (!client.IsConnected)
+        continue;
+    string json = JsonSerializer.Serialize(new {id=Guid.NewGuid(), message = "Heyo :)", sent = DateTimeOffset.UtcNow });
+    await client.PublishAsync(_topic, json);
+    WriteLine("Send Data");
     await Task.Delay(1000);
 }
 static void OnConnected(MqttClientConnectedEventArgs obj)
@@ -37,6 +63,7 @@ static void OnConnected(MqttClientConnectedEventArgs obj)
 static void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
 {
     WriteLine("Couldn't connect to broker.");
+
 }
 
 static void OnDisconnected(MqttClientDisconnectedEventArgs obj)
